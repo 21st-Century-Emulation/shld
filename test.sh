c1d6fc076d60d64ef02b2b7ea51cbcab71739383
David Tyler <david.tyler@metaswitch.com>
2021-04-24 22:37:58 +0100

    Added cpu id

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ docker run --rm --name shld -d -p 8080:8080 -e WRITE_MEMORY_API=http://localhost
 sleep 5
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
-  --data '{"opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":2}}' \
+  --data '{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":2}}' \
   http://localhost:8080/api/v1/execute\?operand2=1\&operand1=3`
-EXPECTED='{"opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":18}}'
+EXPECTED='{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":18}}'
 
 docker kill shld
 
