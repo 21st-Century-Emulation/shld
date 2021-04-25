45f2d862797c27b4d790134f71532029a7f0b014
David Tyler <david.tyler@metaswitch.com>
2021-04-25 16:38:21 +0100

    Added interruptsEnabled flag to cpu state across whole architecture

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ docker run --rm --name shld -d -p 8080:8080 -e WRITE_MEMORY_API=http://localhost
 sleep 5
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
-  --data '{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":2}}' \
+  --data '{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":2,"interruptsEnabled":true}}' \
   http://localhost:8080/api/v1/execute\?operand2=1\&operand1=3`
-EXPECTED='{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":18}}'
+EXPECTED='{"id":"abcd", "opcode":42,"state":{"a":181,"b":0,"c":0,"d":0,"e":0,"h":25,"l":10,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":false,"carry":false},"programCounter":0,"stackPointer":0,"cycles":18,"interruptsEnabled":true}}'
 
 docker kill shld
 
